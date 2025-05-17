8255b6340921d39e140b7f0d21eca79e600889c1
Thiago de Almeida <github@thiagodealmeida.me>
2025-05-16 21:47:36 -0300

    WIP

--- containers/java/entrypoint.sh
+++ containers/java/entrypoint.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-
-# Source SDKMAN
-if [ -f "/root/.sdkman/bin/sdkman-init.sh" ]; then
-  source /root/.sdkman/bin/sdkman-init.sh
-  echo "SDKMAN sourced successfully"
-fi
-
 # Print arguments received
 printf 'Received %d args:' "$#"
 printf " '%s'" "$@"
