7d0fceb497015984744de6d99ab4ed5c79f5e516
Thiago de Almeida <github@thiagodealmeida.me>
2025-05-16 21:19:12 -0300

    WIP

--- containers/java/entrypoint.sh
+++ containers/java/entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Making sdkman available for root user
+echo "source /root/.sdkman/bin/sdkman-init.sh" >> /root/.bashrc
+
 # Print arguments received
 printf 'Received %d args:' "$#"
 printf " '%s'" "$@"
